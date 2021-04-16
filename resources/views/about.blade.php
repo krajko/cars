95349c370f4fb8cc56053d474e270fde9627a2e5
krajko <nikolakrajinovic99@gmail.com>
2021-04-16 18:24:56 +0200

    zadatak 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,6 +4,6 @@
 
 @section ('content')
     <div class="relative flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <h1>Welcome to Cars Project</h1>
+        <h1>About us</h1>
     </div>
 @endsection
